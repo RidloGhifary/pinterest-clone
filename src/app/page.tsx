9a2f0996e7094b7b0f3d849d2ecd814134ec185d
RidloGhifary <ridloghfry@gmail.com>
2024-06-24 16:13:13 +0700

    style: using external api for testing ui homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Pinterest Clone",
   description: "Generated for learning Next.js",
 };
 
-export default function Home() {
+export async function GetPhotos() {
+  try {
+    const res = await fetch("https://picsum.photos/v2/list");
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export default async function Home() {
+  const datas = await GetPhotos();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello, <strong>World!</strong>
-    </h1>
+    <div className="columns-5 gap-3">
+      {datas?.map((data: any) => (
+        <Image
+          src={data?.download_url}
+          alt={data?.author}
+          width={500}
+          height={500}
+          key={data?.id}
+          loading="lazy"
+          className="mb-3 rounded-lg"
+        />
+      ))}
+    </div>
   );
 }
